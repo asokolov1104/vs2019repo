c0818098db74e6f13bfff1fc02d2a19660e3f015
asokolov1104 <asokolov1104@gmail.com>
2020-07-20 11:21:49 +0300

    Warm up project; Virtual Destructor and Direved classes case

---
+++ cnsWarmup/cnsVirtDisrt/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "\t Study case 1\n";
+    class Base
+    {
+    public:
+        virtual ~Base()
+        {
+            std::cout << "calling ~Base()\n";
+        }
+    };
+
+ 
+    class Derived : public Base
+    {
+    public:
+        virtual ~Derived()      
+        {
+            std::cout << "calling ~Derived()\n";
+        }
+    };
+
+
+    Base *Object = new Derived;
+    delete Object;
+
+    std::cout << "\t Study case 2\n";
+
+    class BaseErr
+    {
+    public:
+        ~BaseErr()      //  without virtual ~DirevedErr() won't be called
+        {               //  AND also leads to ..
+            std::cout << "calling ~BaseErr()\n";
+        }
+    };
+
+    class DerivedErr : public BaseErr
+    {
+    public:
+        ~DerivedErr()   //  .. runtime exception (ntdll) when adding virtual keyword here
+        {
+            std::cout << "calling ~DerivedErr()\n";
+        }
+    };
+
+    BaseErr* ObjectErr = new DerivedErr;
+    delete ObjectErr;
+
+    std::cout << "\t Study case 3\n";
+
+    DerivedErr* DerivedObj = new DerivedErr;
+    delete DerivedObj;
+}
